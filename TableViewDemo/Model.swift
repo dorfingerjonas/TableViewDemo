1964827f21a17284c32c06bc9c66e8aadb12ea97
Jonas Dorfinger <j.dorfinger@students.htl-leonding.ac.at>
2020-10-15 09:10:49 +0200

    Initial commit

---
+++ TableViewDemo/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  TableViewDemo
+//
+//  Created by Dorfinger Jonas on 15.10.20.
+//  Copyright © 2020 Dorfinger Jonas. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    var persons = [String]()
+}
---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Dorfinger Jonas on 15.10.20.
+//  Copyright © 2020 Dorfinger Jonas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
